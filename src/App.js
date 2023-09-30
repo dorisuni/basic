185e7dd1ecfeef4d4a0ade6ecb2007b8eb4833e9
dorisuni <daizy13125@gmail.com>
2023-09-30 17:56:03 +0900

    css스타일적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-// import './App.css';
+import './App.css';
 import Component1 from './Component1';
-
+import Header from './Header';
+import Home from './Home';
+import Footer from './Footer';
+import Home2 from './Home2';
+import Home3 from './Home3';
 function App(){
     return(
-        <div className='App'>
-            <h2>Hello React</h2>
-            <Component1 />
+        <div className="App">
+            <Header />
+            <Home />
+            <Footer />
         </div>
     );
 }
